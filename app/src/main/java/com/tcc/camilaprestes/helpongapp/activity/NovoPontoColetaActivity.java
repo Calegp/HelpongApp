d69ed77bf2e9906681f1a1ef08271c533cda3604
Camila Prestes <camila.prestes@yahoo.com.br>
2018-11-18 10:18:02 -0200

    feat: add new menus to ong profile

---
+++ app/src/main/java/com/tcc/camilaprestes/helpongapp/activity/NovoPontoColetaActivity.java
@@ -0,0 +1,15 @@
+package com.tcc.camilaprestes.helpongapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tcc.camilaprestes.helpongapp.R;
+
+public class NovoPontoColetaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_ponto_coleta);
+    }
+}
